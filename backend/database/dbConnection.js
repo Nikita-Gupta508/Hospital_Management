e0f6dc69956539bad784745f9ff8e33b7b956513
Nikita Gupta <nikitagupta582000@gmail.com>
2025-04-28 12:56:22 +0530

    first commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+dotenv.config({});
+
+export const dbConnection = () => {
+   mongoose
+    .connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  })  
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log("Some error occured while connecting to database:", err);
+    });
+};
+
+
---
+++ frontend/src/utilits/constant.js
@@ -0,0 +1,7 @@
+const BASE_URL = "http://localhost:5000/api/v1"
+
+export const SEND_MESSAGE_API = `${BASE_URL}/message/send`;
+
+export const LOGIN_API_END_POINT=`${BASE_URL}/message/send`;
+export const SIGNUP_API_END_POINT=`${BASE_URL}/message/send`;
+export const COMPANY_API_END_POINT=`${BASE_URL}/message/send`;
